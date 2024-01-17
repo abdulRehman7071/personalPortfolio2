bc61d1e0d2be52c557e46678818e9d6c1201bea3
abdulRehman7007 <abdul.rehman@wohlig.com>
2024-01-17 21:11:15 +0530

    Ui changes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,78 +1,79 @@
-import React, { useEffect } from 'react'
-import { useRef } from 'react';
-import { skillsCollection } from '../../utils/data';
+import React, { useEffect } from "react";
+import { useRef } from "react";
+import { skillsCollection } from "../../utils/data";
 import css from "./Skills.module.scss";
-import { motion, useAnimation } from 'framer-motion';
-import { fadeIn, staggerChildren, staggerContainer, textVariant, zoomIn } from '../../utils/motion';
-import { useInView } from 'framer-motion';
+import { motion, useAnimation } from "framer-motion";
+import {
+  fadeIn,
+  staggerChildren,
+  staggerContainer,
+  textVariant,
+  zoomIn,
+} from "../../utils/motion";
+import { useInView } from "framer-motion";
 
 const Skills = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        when: "afterParent",
+      },
+    },
+  };
 
-    const container = {
-        hidden: { opacity: 1, scale: 0 },
-        visible: {
-            opacity: 1,
-            scale: 1,
-            transition: {
-                delayChildren: 0.3,
-                staggerChildren: 0.2,
-
-            }
-        }
-    }
-    const item = {
-        hidden: { y: 20, opacity: 0 },
-        visible: {
-            y: 0,
-            opacity: 1
-        }
-    };
-
-
-
-    return (
-        <motion.section
-            id='skills'
-            variants={staggerContainer}
-            initial="hidden"
-            whileInView="show"
-            viewport={{ once: false, amount: 0.25 }}
-            className={` flexCenter paddings ${css.wrapper}`}>
-
-            <motion.div variants={zoomIn(0, 0.5)} className={css.container}>
-                <div className={` flexCenter ${css.skillsTitle}`}>
-                    <span className="primaryText topPaddings">Skills</span>
-                    <span className="marginTop">Technology Stack</span>
-                </div>
-                <motion.div
-                    variants={container}
-                    initial="hidden"
-                    animate="visible"
-                    // whileInView="show"
-                    className={`${css.skillsCollection}`}>
-                    {
-                        skillsCollection.map((skill, i) => (
-                            // <a href={skill.url} target='_blank'>
-                            <motion.div
-                                viewport={{ once: false, amount: 0.1 }}
-                                key={i}
-                                className={css.icons}
-                                variants={item}
-                            >
-                                <skill.tech size={45} color={skill.color} />
-                                <div className={css.iconsNumber} >
-                                    {skill.proficient}%
-                                </div>
-                            </motion.div>
-
-                            // </a>
-                        ))
-                    }
-                </motion.div>
+  return (
+    <motion.section
+      id="skills"
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={` flexCenter paddings ${css.wrapper}`}
+    >
+      <motion.div variants={zoomIn(0, 0.5)} className={css.container}>
+        <div className={` flexCenter ${css.skillsTitle}`}>
+          <span className="primaryText topPaddings">Skills</span>
+          <span className="marginTop">Technology Stack</span>
+        </div>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          // whileInView="show"
+          className={`${css.skillsCollection}`}
+        >
+          {skillsCollection.map((skill, i) => (
+            // <a href={skill.url} target='_blank'>
+            <motion.div
+              viewport={{ once: false, amount: 0.1 }}
+              key={i}
+              className={css.icons}
+              variants={item}
+            >
+              <skill.tech size={45} color={skill.color} />
+              <div className={css.iconsNumber}>{skill.proficient}%</div>
             </motion.div>
 
-        </motion.section>
-    )
-}
+            // </a>
+          ))}
+        </motion.div>
+      </motion.div>
+    </motion.section>
+  );
+};
 
-export default Skills
+export default Skills;
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,18 @@
 import { FaReact, FaCss3Alt, FaNodeJs, FaBootstrap } from "react-icons/fa";
-import { SiJavascript, SiExpress, SiTailwindcss, SiRedux, SiMongodb, SiNetlify, SiVercel, SiJava, SiPython, SiFirebase, SiGitlab, SiNextdotjs } from "react-icons/si";
+import {
+  SiJavascript,
+  SiExpress,
+  SiTailwindcss,
+  SiRedux,
+  SiMongodb,
+  SiNetlify,
+  SiVercel,
+  SiJava,
+  SiPython,
+  SiFirebase,
+  SiGitlab,
+  SiNextdotjs,
+} from "react-icons/si";
 import { AiFillHtml5 } from "react-icons/ai";
 import { DiSass, DiGit } from "react-icons/di";
 import { GrMysql } from "react-icons/gr";
@@ -10,143 +23,140 @@ export const skillsCollection = [
   {
     tech: SiMongodb,
     proficient: 75,
-    color: '#4BA13E',
-    url: 'https://www.mongodb.com/cloud/atlas/lp/try4?utm_source=bing&utm_campaign=search_bs_pl_evergreen_atlas_core_prosp-brand_gic-null_apac-in_ps-all_desktop_eng_lead&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=415204524&adgroup=1207264237113792&msclkid=bed4db63c2cb109732a5b8b6aa18592a'
+    color: "#4BA13E",
+    url: "https://www.mongodb.com/cloud/atlas/lp/try4?utm_source=bing&utm_campaign=search_bs_pl_evergreen_atlas_core_prosp-brand_gic-null_apac-in_ps-all_desktop_eng_lead&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=415204524&adgroup=1207264237113792&msclkid=bed4db63c2cb109732a5b8b6aa18592a",
   },
   {
     tech: SiExpress,
     proficient: 75,
-    color: '#000000',
+    color: "#000000",
     // url: 'https://www.mongodb.com/cloud/atlas/lp/try4?utm_source=bing&utm_campaign=search_bs_pl_evergreen_atlas_core_prosp-brand_gic-null_apac-in_ps-all_desktop_eng_lead&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=415204524&adgroup=1207264237113792&msclkid=bed4db63c2cb109732a5b8b6aa18592a'
   },
   {
     tech: FaReact,
     proficient: 95,
-    color: '#5ED4F3',
-    url: 'https://reactjs.org/'
+    color: "#5ED4F3",
+    url: "https://reactjs.org/",
   },
   {
     tech: FaNodeJs,
     proficient: 95,
-    color: '#4BA13E',
-    url: 'https://reactjs.org/'
+    color: "#4BA13E",
+    url: "https://reactjs.org/",
   },
   {
     tech: SiNextdotjs,
     proficient: 80,
-    color: '#000000',
-    url: 'https://nextjs.org/'
+    color: "#000000",
+    url: "https://nextjs.org/",
   },
   {
     tech: AiFillHtml5,
     proficient: 95,
-    color: '#F06427',
-    url: 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics'
+    color: "#F06427",
+    url: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
   },
   {
     tech: FaCss3Alt,
     proficient: 95,
-    color: '#2349D7',
-    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
+    color: "#2349D7",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
   },
   {
     tech: SiJavascript,
     proficient: 95,
-    color: '#E8D44D',
-    url: 'https://www.javascript.com/'
+    color: "#E8D44D",
+    url: "https://www.javascript.com/",
   },
 
   {
     tech: SiRedux,
     proficient: 90,
-    color: '#764ABC',
-    url: 'https://redux.js.org/'
+    color: "#764ABC",
+    url: "https://redux.js.org/",
   },
   {
     tech: DiSass,
     proficient: 94,
-    color: '#C76395',
-    url: 'https://sass-lang.com/'
+    color: "#C76395",
+    url: "https://sass-lang.com/",
   },
   {
     tech: SiTailwindcss,
     proficient: 96,
-    color: '#36B7F0',
-    url: 'https://tailwindcss.com/'
+    color: "#36B7F0",
+    url: "https://tailwindcss.com/",
   },
-
 
   {
     tech: FaBootstrap,
     proficient: 80,
-    color: '#7952B3',
-    url: 'https://getbootstrap.com/'
+    color: "#7952B3",
+    url: "https://getbootstrap.com/",
   },
   {
     tech: SiFirebase,
     proficient: 80,
-    color: '#F2C027',
-    url: 'https://firebase.google.com/'
+    color: "#F2C027",
+    url: "https://firebase.google.com/",
   },
 
   {
     tech: SiJava,
     proficient: 85,
-    color: '#0C83BD',
-    url: 'https://www.java.com/en/'
+    color: "#0C83BD",
+    url: "https://www.java.com/en/",
   },
   {
     tech: SiPython,
     proficient: 85,
-    color: '#F2CC38',
-    url: 'https://www.python.org/'
+    color: "#F2CC38",
+    url: "https://www.python.org/",
   },
   {
     tech: SiGitlab,
     proficient: 90,
-    color: '#E24329',
-    url: 'https://about.gitlab.com/'
+    color: "#E24329",
+    url: "https://about.gitlab.com/",
   },
   {
     tech: DiGit,
     proficient: 90,
-    color: '#F03C2E',
-    url: 'https://git-scm.com/'
+    color: "#F03C2E",
+    url: "https://git-scm.com/",
   },
   {
     tech: BsGithub,
     proficient: 90,
-    color: '#000000',
-    url: 'https://github.com/'
+    color: "#000000",
+    url: "https://github.com/",
   },
 
   {
     tech: GrMysql,
     proficient: 85,
-    color: '#D06F30',
-    url: 'https://www.sqltutorial.org/'
+    color: "#D06F30",
+    url: "https://www.sqltutorial.org/",
   },
   {
     tech: SiNetlify,
     proficient: 90,
-    color: '#25C7B7',
-    url: 'https://www.netlify.com/'
+    color: "#25C7B7",
+    url: "https://www.netlify.com/",
   },
   {
     tech: SiVercel,
     proficient: 90,
-    color: '#000000',
-    url: 'https://vercel.com/'
+    color: "#000000",
+    url: "https://vercel.com/",
   },
   {
     tech: FiFigma,
     proficient: 80,
-    color: '#000000',
-    url: 'https://www.figma.com/'
+    color: "#000000",
+    url: "https://www.figma.com/",
   },
-
-]
-
+];
 
 export const projectExperience = [
   {
@@ -171,15 +181,22 @@ export const projectExperience = [
 
 export const About = [
   "Completed 6 Months of Training cum Internship in Frontend Development from Top Centile, Mumbai",
-  'Tech-Stack : HTML, CSS, JavaScript, React.Js, Bootstrap, Tailiwind, Git, Java, Python, SQL.',
+  "Tech-Stack : HTML, CSS, JavaScript, React.Js, Bootstrap, Tailiwind, Git, Java, Python, SQL.",
   "Completed my Engineering from Don Bosco Institute of Technology in 2022",
 ];
 
 export const workExp = [
   {
+    place: "Wohlig Transformation",
+    tenure: "Mar 2022 - Present",
+    role: "Software Development Engineer",
+    detail:
+      "Led end-to-end development of a scalable e-commerce platform frontend, dynamic news portal, and TUI with robust authentication, while also spearheading pixel-perfect websites, an animated DevOps platform, and managing a complex 8-repo project for a fantasy sports company.",
+  },
+  {
     place: "Top Centile",
     tenure: "Jul 2022 - Jan 2023",
-    role: "Frontend Developer Intern",
+    role: "Frontend Developer",
     detail:
       "Worked on Live project of profile management, got trained on frontend technologies like React.JS, JavaScript, HTML5, CSS3, SCSS, Tailwind, Bootstrap, git, gitHub.",
   },
@@ -189,13 +206,6 @@ export const workExp = [
     role: "Engineering Graduate",
     detail:
       "Cultural Secretary, Event and PR Marketing Head of Entrepreneurship Cell,  Robocon 2020 Team Member, Drama Club Member,  National Service Scheme",
-  },
-  {
-    place: "S.M. Shetty Jr. College",
-    tenure: "Jun 2016 - Mar 2018",
-    role: "H.S.C",
-    detail:
-      "A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts",
   },
 ];
 
